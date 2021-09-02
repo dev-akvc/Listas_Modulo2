a78b72a0c1e986f438b3a16d870dfb861035f792
Anna Karolyne <dev.akvc@gmail.com>
2021-09-02 01:46:01 -0300

    Exercicío 13

--- Modulo2_ListaRevisao/src/br/com/zup/Exercicio13.java
+++ Modulo2_ListaRevisao/src/br/com/zup/Exercicio13.java
@@ -1,5 +1,7 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Exercicio13 {
     public static void main(String[] args) {
 /*
@@ -10,5 +12,26 @@ e calcule o excesso. Gravar na variável excesso a quantidade de quilos além do
 e na variável multa o valor da multa que João deverá pagar.
 Imprima os dados do programa com as mensagens adequadas.
  */
+//        Instanciando Scanner
+        Scanner leitor = new Scanner (System.in);
+
+//        Declarando variáveis
+        double pesoPeixe, kgExcedente, precoMulta;
+
+        System.out.println("Olá! Informe quantos kg tem o peixe: ");
+        pesoPeixe = leitor.nextDouble();
+
+        if (pesoPeixe > 50){
+            kgExcedente = pesoPeixe - 50;
+            precoMulta = kgExcedente *4;
+            System.out.println("O regulamento de pesca permite pescar peixes de até 50kg.");
+            System.out.println("O peixe informado tem: " +pesoPeixe);
+            System.out.println("Portanto, excede: " +kgExcedente);
+            System.out.printf("A multa é de R$4,00/kg excedente, totalizando R$ %.2f", precoMulta);
+        }
+        else {
+            System.out.println("O peixe atende ao regulamento de pesca.");
+        }
+
     }
 }
