dd65c951df4a564f829d201d9fafce69ba74d7e8
Anna Karolyne <dev.akvc@gmail.com>
2021-09-02 00:56:51 -0300

    Exercício 8

--- Modulo2_ListaRevisao/src/br/com/zup/Exercicio8.java
+++ Modulo2_ListaRevisao/src/br/com/zup/Exercicio8.java
@@ -1,9 +1,30 @@
 package br.com.zup;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Exercicio8 {
     public static void main(String[] args) {
 /*
 Faça um Programa que leia uma lista de 5 números inteiros e mostre-os.
  */
+//        Instanciando Scanner
+        Scanner leitor = new Scanner(System.in);
+
+//        Declarando variáveis
+        int numeroRecebido = 0;
+        ArrayList<Integer> listaDeNumeros = new ArrayList();
+
+        System.out.println("Informe 5 números inteiros (sem casas decimais): ");
+
+
+        for (int i = 1; i <= 5; i++) {
+            numeroRecebido = leitor.nextInt();
+            listaDeNumeros.add(numeroRecebido);
+        }
+
+
+        System.out.println("Os números informados foram: " +listaDeNumeros);
     }
+
 }
--- Modulo2_ListaRevisao/src/br/com/zup/Exercicio9.java
+++ Modulo2_ListaRevisao/src/br/com/zup/Exercicio9.java
@@ -1,9 +1,18 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Exercicio9 {
     public static void main(String[] args) {
 /*
 Faça um Programa que leia uma lista de 10 números e mostre-os na ordem inversa.
  */
+//        Instanciando Scanner
+        Scanner leitor = new Scanner(System.in);
+
+//        Declarando variáveis
+
     }
+
+
 }
