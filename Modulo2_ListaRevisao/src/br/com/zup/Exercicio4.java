19b41162b68737c1cfeb73d86367bb29e8342119
Anna Karolyne <dev.akvc@gmail.com>
2021-09-02 00:13:11 -0300

    Exercícios 4 e 5

--- Modulo2_ListaRevisao/src/br/com/zup/Exercicio4.java
+++ Modulo2_ListaRevisao/src/br/com/zup/Exercicio4.java
@@ -1,9 +1,26 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Exercicio4 {
     public static void main(String[] args) {
 /*
 Faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
  */
+//        Instanciando Scanner
+        Scanner leitor = new Scanner(System.in);
+
+//        Declarando variáveis
+        int numeroRecebido;
+
+        System.out.println("Informe o número que deseja verificar: ");
+        numeroRecebido = leitor.nextInt();
+        if (numeroRecebido %2 !=0){
+            System.out.println("O número informado é primo!");
+        }
+        else {
+            System.out.println("O número não é primo!");
+        }
+
     }
 }
--- Modulo2_ListaRevisao/src/br/com/zup/Exercicio5.java
+++ Modulo2_ListaRevisao/src/br/com/zup/Exercicio5.java
@@ -1,9 +1,36 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Exercicio5 {
     public static void main(String[] args) {
 /*
 Faça um programa que leia 5 números e informe o maior número.
  */
+//        Instanciando Scanner
+        Scanner leitor = new Scanner(System.in);
+
+//        Declarando variáveis
+        int numeroRecebido, maiorValor = 0;
+
+        System.out.println("Vamos comparar 5 números.");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Informe o " +i+ "ª número: ");
+            numeroRecebido = leitor.nextInt();
+
+            if (i == 1){
+                maiorValor = numeroRecebido;
+            }
+            else {
+                if (numeroRecebido >= maiorValor){
+                    maiorValor = numeroRecebido;
+                }
+
+            }
+        }
+        System.out.println("O maior número é: " +maiorValor);
+
+
     }
 }
