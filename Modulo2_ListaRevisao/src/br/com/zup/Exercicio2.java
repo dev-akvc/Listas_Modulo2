6ab0d1f79aa07990fe7d339ede2fcf7a2e90cb18
Anna Karolyne <dev.akvc@gmail.com>
2021-09-01 23:42:08 -0300

    Exercícios 2 e 3

--- Modulo2_ListaRevisao/src/br/com/zup/Exercicio2.java
+++ Modulo2_ListaRevisao/src/br/com/zup/Exercicio2.java
@@ -1,9 +1,55 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
     public static void main(String[] args) {
 /*
 Faça um Programa que leia um número e exiba o dia correspondente da semana.
  */
+        /* Instanciando Scanner */
+        Scanner leitor = new Scanner(System.in);
+
+//        Declarando variáveis
+        int diaDaSemana;
+
+        System.out.println("Olá, verifique as opções de dias e digite sua escolha:  ");
+        System.out.println("1 - Domingo");
+        System.out.println("2 - Segunda");
+        System.out.println("3 - Terça");
+        System.out.println("4 - Quarta");
+        System.out.println("5 - Quinta");
+        System.out.println("6 - Sexta");
+        System.out.println("7 - Sábado");
+
+        diaDaSemana = leitor.nextInt();
+//        System.out.println("A opção escolhida foi " + diaDaSemana);
+
+        switch (diaDaSemana){
+            case 1:
+                System.out.println("Domingoooou!");
+                break;
+            case 2:
+                System.out.println("Segundou!");
+                break;
+            case 3:
+                System.out.println("Terçou!");
+                break;
+            case 4:
+                System.out.println("Quartou!");
+                break;
+            case 5:
+                System.out.println("Quintou!");
+                break;
+            case 6:
+                System.out.println("Sextooou!");
+                break;
+            case 7:
+                System.out.println("Sabadooou!");
+                break;
+            default:
+                System.out.println("Digite uma opção válida.");
+                break;
+       }
     }
 }
--- Modulo2_ListaRevisao/src/br/com/zup/Exercicio3.java
+++ Modulo2_ListaRevisao/src/br/com/zup/Exercicio3.java
@@ -1,5 +1,7 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Exercicio3 {
     public static void main(String[] args) {
 /*
@@ -7,5 +9,19 @@ Desenvolva um gerador de tabuada, capaz de gerar a tabuada
 de qualquer número inteiro entre 1 a 10. O usuário deve informar de qual
 número ele deseja ver a tabuada.
  */
+//        Instanciando Scanner
+        Scanner leitor = new Scanner(System.in);
+
+//        Declarando variáveis
+        int numeroDaTabuada, tabuada;
+
+        System.out.println("Olá! Quer a tabuada de qual número?");
+        numeroDaTabuada = leitor.nextInt();
+
+        for (int i = 0; i <= 10; i++) {
+            tabuada = numeroDaTabuada * i;
+            System.out.println(numeroDaTabuada+ " x " +i+ " = " +tabuada);
+        }
+
     }
 }
