227a4a38fe29d49213cf82a38ced04fe9ed2843a
Anna Karolyne <dev.akvc@gmail.com>
2021-09-02 00:31:57 -0300

    Exercícios 6 e 7

--- Modulo2_ListaRevisao/src/br/com/zup/Exercicio6.java
+++ Modulo2_ListaRevisao/src/br/com/zup/Exercicio6.java
@@ -5,5 +5,15 @@ public class Exercicio6 {
 /*
 Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
  */
+//        Declarando variáveis
+        int numeroParaVerificar;
+
+        System.out.println("O número é ímpar?");
+
+        for (int i = 1; i < 50; i++) {
+            if (i % 2 != 0){
+                System.out.println(i+ " é ímpar");
+            }
+        }
     }
 }
--- Modulo2_ListaRevisao/src/br/com/zup/Exercicio7.java
+++ Modulo2_ListaRevisao/src/br/com/zup/Exercicio7.java
@@ -1,10 +1,33 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Exercicio7 {
     public static void main(String[] args) {
 /*
 Faça um programa que receba dois números inteiros e gere os números inteiros
 que estão no intervalo compreendido por eles.
  */
+//        Instanciando Scanner
+        Scanner leitor = new Scanner(System.in);
+
+//        Declarando variáveis
+        int numeroInicial = 0, numeroFinal = 0;
+
+        System.out.println("Olá! Informe o primeiro número: ");
+        numeroInicial = leitor.nextInt();
+        System.out.println("Informe o último número: ");
+        numeroFinal = leitor.nextInt();
+
+        if (numeroInicial <= numeroFinal) {
+            for (int i = numeroInicial; i <= numeroFinal; i++) {
+                System.out.println(i);
+            }
+        }
+        else {
+            System.out.println("Digite um intervalo válido.");
+        }
+
     }
 }
+
